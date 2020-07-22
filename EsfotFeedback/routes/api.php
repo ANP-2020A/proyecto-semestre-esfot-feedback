0c318d6faefa32361b4bc7900c74987de24780bc
Sebastian <sebastian.morales@epn.edu.ec>
2020-07-22 01:07:51 -0500

    Uniendo la rama con develop

--- EsfotFeedback/routes/api.php
+++ EsfotFeedback/routes/api.php
@@ -14,17 +14,13 @@ use Illuminate\Http\Request;
 |
 */
 
-<<<<<<< HEAD
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
 
 Route::get('subjects', 'SubjectController@index');
 Route::get('subjects/{subject}', 'SubjectController@show');
 Route::post('subjects', 'SubjectController@store');
 Route::put('subjects/{subject}', 'SubjectController@update');
 Route::delete('subjects/{subject}', 'SubjectController@delete');
-=======
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -33,4 +29,4 @@ Route::get('answers', 'AnswerController@index');
 Route::get('answers/{answer}', 'AnswerController@show');
 Route::post('answers', 'AnswerController@store');
 Route::delete('answers/{answer}', 'AnswerController@delete');
->>>>>>> 422b8cef8f7e864e8b23793a0f6cf0b91864c2db
+
