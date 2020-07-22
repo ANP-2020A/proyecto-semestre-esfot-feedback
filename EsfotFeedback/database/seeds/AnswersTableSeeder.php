8eec6451cf3712851bfb4c557c3b569a59f9b936
XavierCalle <xav.xgca@gmail.com>
2020-07-21 23:34:50 -0500

    #12 Creacion modelo, migracion y seeder Answer

---
+++ EsfotFeedback/database/seeds/AnswersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Answer;
+use Illuminate\Database\Seeder;
+
+class AnswersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Vaciar la tabla.
+        Answer::truncate();
+        $faker = \Faker\Factory::create();
+        // Crear artículos ficticios en la tabla
+        for ($i = 0; $i < 5; $i++) {
+            Answer::create([
+                'Fk_idQuestion' =>  $faker->numberBetween(1,5),
+                'FK_idUser' => $faker->uuid,
+                'FK_idChapter' => $faker->uuid,
+                'Value' => $faker->numberBetween(1,5)
+            ]);
+        }
+    }
+}
