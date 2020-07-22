68fe6e713d2fd7fb9cc44791b145fc4daf2392de
Sebastian <sebastian.morales@epn.edu.ec>
2020-07-22 00:31:24 -0500

    #14 creación de rutas para Subject

--- EsfotFeedback/routes/api.php
+++ EsfotFeedback/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Subject;
 use Illuminate\Http\Request;
 
 /*
@@ -13,6 +14,29 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('subjects', function() {
+    return Subject::all();
+});
+
+Route::get('subjects/{id}', function($id) {
+    return Subject::find($id);
+});
+
+Route::post('subjects', function(Request $request) {
+    return Subject::create($request->all());
+});
+
+Route::put('subjects/{id}', function(Request $request, $id) {
+    $article = Subject::findOrFail($id);
+    $article->update($request->all());
+    return $article;
+});
+
+Route::delete('subjects/{id}', function($id) {
+    Subject::find($id)->delete();
+    return 204;
 });
