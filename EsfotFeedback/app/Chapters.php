733c7d42b2d6c112a05bfce3a3145ced7add82bf
XavierCalle <xav.xgca@gmail.com>
2020-07-25 15:37:51 -0500

    #27 Relacion subject-chapters

--- EsfotFeedback/app/Chapters.php
+++ EsfotFeedback/app/Chapters.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Chapters extends Model
 {
     protected $fillable = ['Topic', 'Objetives'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Subject','FK_idSubject','id');
+    }
 }
