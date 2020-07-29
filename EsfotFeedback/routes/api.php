4575cfe1e5d612154d2a5e67d5603fedcd651a56
xav.xgca@gmail.com <xav.xgca@gmail.com>
2020-07-28 22:27:49 -0500

    Cambio rutas de SubjectUser

--- EsfotFeedback/routes/api.php
+++ EsfotFeedback/routes/api.php
@@ -21,12 +21,6 @@ Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@authenticate');
 
 
-//SubjectUsers
-Route::get('subject_Users', 'SubjectUserController@index');
-Route::get('subject_Users/{subject_Users}', 'SubjectUserController@show');
-Route::post('subject_Users', 'SubjectUserController@store');
-Route::delete('subject_Users/{subject_Users}', 'SubjectUserController@delete');
-
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('user', 'UserController@getAuthenticatedUser');
 
@@ -55,6 +49,10 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     Route::post('questions', 'QuestionController@store');
     Route::delete('questions/{question}', 'QuestionController@delete');
 
-
+    //SubjectUsers
+Route::get('subject_Users', 'SubjectUserController@index');
+Route::get('subject_Users/{subject_Users}', 'SubjectUserController@show');
+Route::post('subject_Users', 'SubjectUserController@store');
+Route::delete('subject_Users/{subject_Users}', 'SubjectUserController@delete');
 
 });
