45807aca513501f552b892a19d4d9e7dd6d02c63
MARIO BENJAMIN MONTERO CEDILLO <mario.montero@epn.edu.ec>
2020-07-28 20:38:34 -0500

    #45#41 creacion recursos answer-question

--- EsfotFeedback/app/Http/Controllers/AnswerController.php
+++ EsfotFeedback/app/Http/Controllers/AnswerController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Answer;
+
 use Illuminate\Http\Request;
+use App\Http\Resources\answer as AsnwerResource;
 
 class AnswerController extends Controller
 {
     public function index()
     {
-        return Answer::all();
+        return AsnwerResource::collection(Answer::all());
     }
     public function show(Answer $answer)
     {
-        return $answer;
+        return new AsnwerResource($answer);
     }
     public function store(Request $request)
     {
